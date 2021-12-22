8fc96ae20504df3430d616e9e4729ffb740e86be
jfranco9202 <jeremy.franco41@yahoo.com>
2021-12-22 11:31:47 -0600

    Switch Statements

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "Math";
+
+            Console.WriteLine("What is you favorite school subject?");
+            string userSubject = Console.ReadLine();
+
+            switch(userSubject)
+            {
+                case "Math":
+                    Console.WriteLine("You are correct about the subject");
+                    break;
+                case "Java":
+                    Console.WriteLine("You are incorrect.");
+                    break;
+                case "English":
+                    Console.WriteLine("You are incorrect");
+                    break;
+                case "History":
+                    Console.WriteLine("You are incorrect");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("You are incorrect");
+                    break;
+
+                default:
+                    Console.WriteLine("We teach math here.");
+                    break;
+
+            }
+
+
+
+
+            
+        }
+    }
+}
